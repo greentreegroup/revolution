72577eea24686a9603d842b4dc47d4fac1efe68f
DChilJr <drechiltonjr03@gmail.com>
2023-06-30 15:16:46 -0400

    Website CSS and JS code added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*-----Javascript for Project Name */
+$(document).ready(function() {
+   // Fakes the loading setting a timeout
+   setTimeout(function() {
+      $('body').addClass('loaded');
+   }, 1500);
+});
